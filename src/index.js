7f87e29a89012fa5a2f230df60106012a821bfde
Zack Story <ztstory@gmail.com>
2016-04-30 21:04:47 -0700

    fix version check

--- src/index.js
+++ src/index.js
@@ -21,10 +21,10 @@ export default function createMigration (manifest, versionSelector, versionSette
     return (state, action) => {
       if (action.type !== REHYDRATE) return reducer(state, action)
       else {
+        let incomingState = action.payload
         let incomingVersion = versionSelector(incomingState)
         if (incomingVersion === currentVersion) return reducer(state, action)
 
-        let incomingState = action.payload
         let migratedState = migrate(incomingState, incomingVersion)
         action.payload = migratedState
         return reducer(migratedState, action)
@@ -34,7 +34,7 @@ export default function createMigration (manifest, versionSelector, versionSette
 
   function migrate (state, version) {
     versionKeys
-      .filter((v) => !currentVersion || v > currentVersion)
+      .filter((v) => !version || v > version)
       .forEach((v) => { state = manifest[v](state) })
     state = versionSetter(state, currentVersion)
     return state
