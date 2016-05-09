79289dbb2764ad98a36b0002a147fe708ce3b592
Zack Story <ztstory@gmail.com>
2016-05-09 16:30:29 -0700

    wrap dispatch instead of reducer

--- src/index.js
+++ src/index.js
@@ -18,30 +18,32 @@ export default function createMigration (manifest, versionSelector, versionSette
   const versionKeys = Object.keys(manifest).sort()
   const currentVersion = versionKeys[versionKeys.length - 1]
 
-  return (next) => (reducer, initialState, enhancer) => {
-    return next(createMigrationReducer(reducer), initialState, enhancer)
-  }
-
-  function createMigrationReducer (reducer) {
-    return (state, action) => {
-      if (action.type !== REHYDRATE) return reducer(state, action)
-      else {
-        let incomingState = action.payload
-        let incomingVersion = versionSelector(incomingState)
-        if (incomingVersion === currentVersion) return reducer(state, action)
+  const migrationDispatch = (next) => (action) => {
+    if (action.type === REHYDRATE) {
+      let incomingState = action.payload
+      let incomingVersion = versionSelector(incomingState)
+      if (incomingVersion !== currentVersion) {
 
         let migratedState = migrate(incomingState, incomingVersion)
         action.payload = migratedState
-        return reducer(migratedState, action)
       }
     }
+    return next(action)
   }
 
-  function migrate (state, version) {
+  const migrate = (state, version) => {
     versionKeys
       .filter((v) => !version || v > version)
       .forEach((v) => { state = manifest[v](state) })
     state = versionSetter(state, currentVersion)
     return state
+  }
+
+  return (createStore) => (reducer, initialState, enhancer) => {
+    var store = createStore(reducer, initialState, enhancer)
+    return {
+      ...store,
+      dispatch: migrationDispatch(store.dispatch)
+    }
   }
 }
