60a378ec102496f9f6004a82b2a8bdde20bc87ac
Zack Story <zack@root-two.com>
2016-08-16 16:52:49 -0700

    [v4] integer versions, readme fixes

--- README.md
+++ README.md
@@ -4,24 +4,26 @@ Migrate redux state between versions with redux-persist.
 
 #### Usage
 ```js
+import { compose, createStore } from 'redux'
 import { persistStore, autoRehydrate } from 'redux-persist'
 import createMigration from 'redux-persist-migrate'
 
 const manifest = {
- 0.1: (state) => ({...state, staleReducer: undefined})
- 0.2: (state) => ({...state, app: {...state.app, staleKey: undefined}})
+ 1: (state) => ({...state, staleReducer: undefined})
+ 2: (state) => ({...state, app: {...state.app, staleKey: undefined}})
 }
 
 // reducerKey is the key of the reducer you want to store the state version in
 // in this example after migrations run `state.app.version` will equal `0.2`
 let reducerKey = 'app'
 const migration = createMigration(manifest, reducerKey)
+const enhancer =  compose([migration, autoRehydrate()])
 
-const store = createStore(reducer, null, migration(autoRehydrate()))
+const store = createStore(reducer, null, enhancer)
 persistStore(store)
 ```
 
-In the above example `migration = createMigration(manfiest, 'app')` is equivalent to the more generalized syntax:
+In the above example `migration = createMigration(manifest, 'app')` is equivalent to the more generalized syntax:
 ```js
 // alternatively with version selector & setter
 const migration = createMigration(
--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 import { REHYDRATE } from 'redux-persist/constants'
 
+const processKey = (key) => {
+  let int = parseInt(key)
+  if (isNaN(int)) throw new Error('redux-persist-migrate: migrations must be keyed with integer values')
+  return int
+}
+
 export default function createMigration (manifest, versionSelector, versionSetter) {
   if (typeof versionSelector === 'string') {
-    let versionString = versionSelector
-    versionSelector = (state) => state && state[versionString] && state[versionString].version
+    let reducerKey = versionSelector
+    versionSelector = (state) => state && state[reducerKey] && state[reducerKey].version
     versionSetter = (state, version) => {
-      if (['undefined', 'object'].indexOf(typeof state[versionString]) === -1) {
+      if (['undefined', 'object'].indexOf(typeof state[reducerKey]) === -1) {
         console.error('redux-persist-migrate: state for versionSetter key must be an object or undefined')
         return state
       }
-      state[versionString] = state[versionString] || {}
-      state[versionString].version = version
+      state[reducerKey] = state[reducerKey] || {}
+      state[reducerKey].version = version
       return state
     }
   }
 
-  const versionKeys = Object.keys(manifest).sort()
+  const versionKeys = Object.keys(manifest).map(processKey).sort()
   const currentVersion = versionKeys[versionKeys.length - 1]
 
   const migrationDispatch = (next) => (action) => {
@@ -23,7 +29,6 @@ export default function createMigration (manifest, versionSelector, versionSette
       let incomingState = action.payload
       let incomingVersion = versionSelector(incomingState)
       if (incomingVersion !== currentVersion) {
-
         let migratedState = migrate(incomingState, incomingVersion)
         action.payload = migratedState
       }
@@ -39,8 +44,8 @@ export default function createMigration (manifest, versionSelector, versionSette
     return state
   }
 
-  return (createStore) => (reducer, initialState, enhancer) => {
-    var store = createStore(reducer, initialState, enhancer)
+  return (next) => (reducer, initialState, enhancer) => {
+    var store = next(reducer, initialState, enhancer)
     return {
       ...store,
       dispatch: migrationDispatch(store.dispatch)
