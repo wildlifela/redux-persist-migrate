f87b0b8ba946f35d1d9ba045396e5130cae16e93
Joël Charles <joel@getstation.com>
2017-05-17 12:18:01 +0200

    Immutable.js support

--- src/index.js
+++ src/index.js
@@ -1,24 +1,62 @@
 import { REHYDRATE } from 'redux-persist/constants'
+import Immutable from 'immutable'
 
 const processKey = (key) => {
-  let int = parseInt(key)
+  const int = parseInt(key)
   if (isNaN(int)) throw new Error('redux-persist-migrate: migrations must be keyed with integer values')
   return int
 }
 
-export default function createMigration (manifest, versionSelector, versionSetter) {
-  if (typeof versionSelector === 'string') {
-    let reducerKey = versionSelector
-    versionSelector = (state) => state && state[reducerKey] && state[reducerKey].version
-    versionSetter = (state, version) => {
-      if (['undefined', 'object'].indexOf(typeof state[reducerKey]) === -1) {
-        console.error('redux-persist-migrate: state for versionSetter key must be an object or undefined')
+const isKeyValid = (key) => {
+  if (['undefined', 'object'].indexOf(typeof key) === -1) {
+    console.error('redux-persist-migrate: state for versionSetter key must be an object or undefined')
+    return false
+  }
+  return true
+}
+
+const getVersionSelector = (reducerKey) => {
+  return (state) => {
+    if (Immutable.Map.isMap(state)) {
+      return state.getIn([reducerKey, 'version'], null)
+    }
+    if (reducerKey in state && 'version' in state[reducerKey]) {
+      return state[reducerKey].version
+    }
+    return null
+  }
+}
+
+const getVersionSetter = (reducerKey) => {
+  return (state, version) => {
+    if (Immutable.Map.isMap(state)) {
+      const reduced = state.get(reducerKey)
+      if (!isKeyValid(reduced)) {
         return state
       }
-      state[reducerKey] = state[reducerKey] || {}
-      state[reducerKey].version = version
+      if (!state.has(reducerKey)) {
+        state.set(reducerKey, Immutable.Map())
+      }
+      return state.setIn([reducerKey, 'version'], version)
+    }
+    if (!isKeyValid(state[reducerKey])) {
       return state
     }
+    return {
+      ...state,
+      [reducerKey]: {
+        ...state[reducerKey],
+        version: version
+      }
+    }
+  }
+}
+
+export default function createMigration (manifest, versionSelector, versionSetter) {
+  if (typeof versionSelector === 'string') {
+    const reducerKey = versionSelector
+    versionSelector = getVersionSelector(reducerKey)
+    versionSetter = getVersionSetter(reducerKey)
   }
 
   const versionKeys = Object.keys(manifest).map(processKey).sort((a, b) => a - b)
@@ -43,14 +81,16 @@ export default function createMigration (manifest, versionSelector, versionSette
     if (version != null) {
       versionKeys
         .filter((v) => v > version)
-        .forEach((v) => { state = manifest[v](state) })
+        .forEach((v) => {
+          state = manifest[v](state)
+        })
     }
     state = versionSetter(state, currentVersion)
     return state
   }
 
   return (next) => (reducer, initialState, enhancer) => {
-    var store = next(reducer, initialState, enhancer)
+    const store = next(reducer, initialState, enhancer)
     return {
       ...store,
       dispatch: migrationDispatch(store.dispatch)
